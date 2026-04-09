6d570e53a43440fef683088f79f73b70f0176119
Angela McLeary <angela.mcleary@outlook.com>
2026-04-09 20:04:40 +0100

    update validation requiremtns on lines 36-41

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -31,10 +31,14 @@ function submit() {
   if (
     title.value == null ||
     title.value == "" ||
+    author.value == null ||
+    author.value == "" ||
     pages.value == null ||
-    pages.value == ""
+    pages.value == "" ||
+    pages.value <= 0 ||
+    pages.value != parseInt() 
   ) {
-    alert("Please fill all fields!");
+    alert("Please fill all fields with valid input!");
     return false;
   } else {
     let book = new Book(title.value, author.value, pages.value, check.checked);
